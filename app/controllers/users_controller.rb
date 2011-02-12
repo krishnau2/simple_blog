ca87bd7d2792a514c988068b8783276b48a386ce
krishna kumar <krishnau2@gmail.com>
2011-02-13 02:27:10 +0530

    Post creator's name_id is taken from the user table for the current logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   def index
     list_posts = Post.new
     @current_user      = session[:current_user]
+    @current_user_id = User.find_by_name(@current_user)
     #collecting current logged in user posts.
-    @current_user_post = list_posts.get_current_user_post(@current_user)
+    @current_user_post = list_posts.get_current_user_post(@current_user_id)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ActiveRecord::Base
-  validates :name, :presence => true
   validates :topic, :presence => true
 
   belongs_to :users
   has_many :comments
   has_attached_file :attachment
 
-  def get_current_user_post(current_user)
-    return Post.where("name = ? ",current_user)
+  def get_current_user_post(current_user_id)
+    return Post.where("name_id = ? ",current_user_id)
   end
 end
