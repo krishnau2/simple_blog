361de4b1a745c991e61489d77fe11fdb21b725a8
krishna kumar <krishnau2@gmail.com>
2011-02-20 10:44:38 +0530

    Added some comments in authentication control section

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,20 +5,24 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
-#    render :text => request.env["omniauth.auth"].to_yaml
+#    collecting the authenticated user-details returned by  facebook.
     facebook_result           = request.env["omniauth.auth"]
 
     @name                     = facebook_result['user_info']['name']
     uid                       = facebook_result['uid']
     provider                  = facebook_result['provider']
+#    storing loggedIn user details in session variables for logIn session management
     session[:current_user]    = @name
     session[:current_user_id] = uid
+
     existing_user             = User.find_by_uid(uid)
     if existing_user
-        flash[:notice]                  = "Old user #{@name} Signed in Successfully"
+      flash[:notice]                  = "Old user #{@name} Signed in Successfully"
+#      Storing the loggedIn user category for identifying and managing  user-roles.
       session[:current_user_category] = existing_user.category
       redirect_to "/posts"
     else
+#      For a new user the user_category will be general
       new_user                        = User.create(:name => @name, :category => "general", :provider => provider, :uid => uid)
       session[:current_user_category] = "general"
       flash[:notice]                  = "New user #{@name} Signed in Successfully"
@@ -28,9 +32,9 @@ class AuthenticationsController < ApplicationController
   end
 
   def logout
-    session[:current_user] = ""
-    session[:current_user_id] = ""
-    session[:login_status] = "loggedOut"
+    session[:current_user]          = ""
+    session[:current_user_id]       = ""
+    session[:login_status]          = "loggedOut"
     session[:current_user_category] = ""
     redirect_to "/home"
   end
